1b7fb021dbf00aa1bd25cb3cac54a36a31252f47
karthik2804 <karthik.ganeshram@fermyon.com>
2023-04-01 17:20:30 -0700

    check for valid status
    Signed-off-by: karthik2804 <karthik.ganeshram@fermyon.com>

--- redirect.go
+++ redirect.go
@@ -8,9 +8,19 @@ import (
 func main() {
 	dest := os.Getenv("DESTINATION")
 	status := os.Getenv("STATUS")
-	if status != "" {
+	if status != "" && is_valid_status(status) {
 		fmt.Printf("status: %s\nlocation: %s\n\n", status, dest)
 	} else {
-		fmt.Printf("status: 302\nlocation: %s\n\n", dest)
+		fmt.Printf("status: 302\nlocation1: %s\n\n", dest)
 	}
+}
+
+func is_valid_status(status string) bool {
+	allowed_status := []string{"301", "302", "303", "307", "308"}
+	for _, v := range allowed_status {
+		if v == status {
+			return true
+		}
+	}
+	return false
 }
